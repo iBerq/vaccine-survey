88b2d78774413e9e1edffb1d79f325afe6f008b2
ege-sahin <sahin.ege98@gmail.com>
2022-03-29 01:30:39 +0300

    Test Case 1 Works Fine Test Case 2 and 3 problematic

--- appium/TestAppium.test.js
+++ appium/TestAppium.test.js
@@ -1,6 +1,6 @@
 import { remote } from "webdriverio";
 
-jest.setTimeout(60000);
+jest.setTimeout(20000);
 let driver;
 
 beforeAll(async () => {
@@ -24,27 +24,73 @@ afterAll(async () => {
 });
 
 
-test("Open Form Test", async () => {
+test("Form Test", async () => {
   await driver.pause(2000);
 
+  /*
+  // Test Case 1 - Try to Click Submit Button Before Filling the Form Fields
   const filltheFormButton = await driver.$("~FilltheForm");
   await filltheFormButton.waitForExist();
   filltheFormButton.click();
   await driver.pause(3000);
-  
-  /*const loginUsernameInput = await driver.$("~LoginInput");
-  await loginUsernameInput.clearValue();
-  await loginUsernameInput.setValue("TestAccount@test.com");
-  await driver.hideKeyboard("pressKey", "next");
 
-  const loginNextButton = await driver.$("~LoginNextButton");
-  await loginNextButton.waitForExist();
-  loginNextButton.click();
+  const submitButton = await driver.$("~SubmitForm");
+  await submitButton.waitForExist();
+  submitButton.click();
   await driver.pause(3000);
 
-  const loginPasswordInput = await driver.$("~LoginPasswordInput");
-  await loginPasswordInput.setValue("Test1234");
-  const loginButton = await driver.$("~LoginButton");
-  await loginButton.click();
-  await driver.pause(3000);*/
+  const backToHomeScreenButton = await driver.$("~Navigate up");
+  await backToHomeScreenButton.waitForExist();
+  backToHomeScreenButton.click();
+  await driver.pause(3000);
+  // ----------------------------------------------------------------------
+  */
+
+  // Test Case 2 - Click Edit Form Button After Filling the Form and See Whether the Form is Correct or not
+  const filltheFormButton = await driver.$("~FilltheForm");
+  await filltheFormButton.waitForExist();
+  filltheFormButton.click();
+  await driver.pause(3000);
+
+  const nameInput = await driver.$("~NameField");
+  await nameInput.clearValue();
+  await nameInput.setValue("Ege");
+
+  const SurnameInput = await driver.$("~SurnameField");
+  await SurnameInput.clearValue();
+  await SurnameInput.setValue("Sahin");
+
+  const BirthDateSelect = await driver.$("~BirthDateSelect");
+
+  const CitySelect = await driver.$("~CitySelect");
+  await CitySelect.waitForExist();
+  CitySelect.click();
+
+  const GenderSelect = await driver.$("~GenderSelect");
+  const VaccineSelect = await driver.$("~VaccineSelect");
+  const SideEffectSelect = await driver.$("~SideEffectSelect");
+  const PCRPosSelect = await driver.$("~PCRPosSelect");
+  
+  const backToHomeScreenButton = await driver.$("~Navigate up");
+  await backToHomeScreenButton.waitForExist();
+  backToHomeScreenButton.click();
+  await driver.pause(3000);
+  // ----------------------------------------------------------------------
+
+  /*
+  // Test Case 3 - Entering Numbers For Name and Surname Fields
+  const filltheFormButton = await driver.$("~FilltheForm");
+  await filltheFormButton.waitForExist();
+  filltheFormButton.click();
+  await driver.pause(3000);
+
+  const nameInput = await driver.$("~NameField");
+  await nameInput.clearValue();
+  await nameInput.setValue("Ege");
+
+  const SurnameInput = await driver.$("~SurnameField");
+  await SurnameInput.clearValue();
+  await SurnameInput.setValue("Sahin");
+   // ----------------------------------------------------------------------
+  */
 });
